9b97d8780bd4fc53c67e32b4d7ded3680028c513
Amanda <freitasamandalais@gmail.com>
2024-08-23 21:58:55 -0300

    alguns outros

--- Aula 03/EXERCICIOS AULA/Carro.java
+++ Aula 03/EXERCICIOS AULA/Carro.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Carro {
+public class Carro {
+    public String modelo;
+    public String marca;
+    public int ano;
+    public String cor;
+    public int potencia;
+    public double valor;
+    public int vMaxima;
+
+    public Carro(String modelo, String marca, int ano, String cor, int potencia, double valor, int vMaxima) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.cor = cor;
+        this.potencia = potencia;
+        this.valor = valor;
+        this.vMaxima = vMaxima;
+    }
+
+    @Override
+    public String toString() {
+        return marca + " " + modelo + " (" + ano + "), " + cor.toUpperCase() +
+                ", Potência: " + potencia + " cv, Valor: R$" + valor +
+                ", Velocidade Máxima: " + vMaxima + " Km/h";
+
+    }
+
+    public void acelerar (int incremento) {
+        System.out.println(marca + " " + modelo + " acelerando " + incremento + " Km/h.");
+    }
+    public void frear (int decremento) {
+        System.out.println(marca + " " + modelo + "freando " + decremento + " Km/h.");
+    }
 }
+
--- Aula 03/EXERCICIOS AULA/Main.java
+++ Aula 03/EXERCICIOS AULA/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Carro bmwAzul = new Carro("i4", "BMW", 2020, "Azul",286, 469.950, 225);
+        Carro skylineRosa = new Carro("R34", "Nissan", 1998, "Rosa", 280, 2.800000, 327);
+        Carro silviaRoxo = new Carro("S15", "Nissan", 2001, "Roxo", 250, 180.000,350);
+        Carro supraRoxo = new Carro("Mk4", "Toyota", 1993, "Roxo", 326, 250.000, 250);
+        System.out.println(bmwAzul.toString());
+    }
 }
